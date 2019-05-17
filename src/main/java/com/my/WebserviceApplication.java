e746c9e555dd7973d73757f03080b41a323f6645
yddjyy <18839609272@163.com>
2019-05-17 22:58:06 +0800

    添加事务管理注解

--- src/main/java/com/my/WebserviceApplication.java
+++ src/main/java/com/my/WebserviceApplication.java
@@ -2,8 +2,10 @@ package com.my;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class WebserviceApplication {
 
     public static void main(String[] args) {
